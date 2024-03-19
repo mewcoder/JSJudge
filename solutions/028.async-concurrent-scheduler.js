a78c24e48ac46ab88aa630da412a54f5f7c11d7a
mewcoder <1010953107@qq.com>
2024-03-19 23:35:16 +0800

    feat: update

--- solutions/028.async-concurrent-scheduler.js
+++ solutions/028.async-concurrent-scheduler.js
@@ -1,86 +1,78 @@
-// class Scheduler {
-//     constructor(max) {
-//         this.max = max
-//         this.queue = []
-//         this.count = 0
-//     }
-//     add(task) {
-//         return new Promise(resolve => {
-//             // 关键，保存 resolve
-//             task.resolve = resolve
-//             if (this.count < this.max) {
-//                 this.run(task)
-//             } else {
-//                 this.queue.push(task)
-//             }
-//         })
-//     }
-//     async run(task) {
-//         if (task && typeof task === 'function') {
-//             this.count++
-//             await task()
-//             task.resolve()
-//             this.count--
-//             const fn = this.queue.shift()
-//             this.run(fn)
-//         }
-//     }
-// }
-
-
 class Scheduler {
-    constructor(n) {
-        this.max = n || 2;
-        this.count = 0;
-        this.queue = []; // 当前执行任务队列
-    }
-
-    add(task) {
-        this.queue.push(task);
-        this.run();
-    }
-
-    run() {
-        if (this.count >= this.max || this.queue.length === 0) return
-        this.count++;
-
-        const fn = this.queue.shift();
-        fn.then(() => {
-            this.count--;
-            this.run();
-        })
-    }
+  constructor(max = 2) {
+    this.max = max;
+    this.queue = [];
+    this.count = 0;
+  }
+  add(task) {
+    return new Promise((resolve) => {
+      // 关键，保存 resolve
+      task.resolve = resolve;
+      this.queue.push(task);
+      this.run();
+    });
+  }
+  run() {
+    if (this.count >= this.max || this.queue.length === 0) return;
+    this.count++;
+    const fn = this.queue.shift();
+    fn()
+      .then(() => {
+        fn.resolve();
+        this._next();
+      })
+      .catch(() => {
+        this._next();
+      });
+  }
+  _next() {
+    this.count--;
+    this.run();
+  }
 }
-// https://juejin.cn/post/7093213891198451742
+// class Scheduler {
+//   constructor(max=2) {
+//     this.max = max;
+//     this.queue = [];
+//     this.running = 0;
+//   }
+//   add(task) {
+//     return new Promise((resolve) => {
+//       // 关键，加了一层 Promise 的封装，将其 resolve 的函数交出去
+//       task.resolve = resolve;
+//       if (this.running < this.max) {
+//         this.run(task);
+//       } else {
+//         this.queue.push(task);
+//       }
+//     });
+//   }
+//   async run(task) {
+//     if (task && typeof task === "function") {
+//       this.running++;
+//       await task();
+//       task.resolve();
+//       this.running--;
+//       const fn = this.queue.shift();
+//       this.run(fn);
+//     }
+//   }
+// }
 
 // 测试
 const timeout = (time) =>
-    new Promise((resolve) => {
-        setTimeout(resolve, time);
-    });
+  new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
 
 const scheduler = new Scheduler();
-// const addTask = (time, order) => {
-//     // scheduler.add(() => timeout(time)).then(() => console.log(order));
-// };
-
-// addTask(1000, '1');
-// addTask(500, '2');
-// addTask(300, '3');
-// addTask(400, '4');
-
-
-const curl = (i) => {
-    console.log("开始" + i);
-    return new Promise((resolve) =>
-        setTimeout(() => {
-            resolve(i);
-            console.log("结束" + i);
-        }, 1000 + Math.random() * 1000)
-    );
+const addTask = (time, order) => {
+  scheduler.add(() => timeout(time)).then(() => console.log(order));
 };
 
-scheduler.add(curl(2))
-scheduler.add(curl(3))
-scheduler.add(curl(4))
-scheduler.add(curl(2))
+addTask(1000, "1");
+addTask(500, "2");
+addTask(300, "3");
+addTask(400, "4");
+
+// 输出: 2 3 1 4
